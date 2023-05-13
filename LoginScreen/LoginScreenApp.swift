a870850e1ec9aafc07450b81c33f61b778e6efd9
Mert Aydogan <mertaydogn0@gmail.com>
2023-05-14 02:24:58 +0300

    Initial Commit

---
+++ LoginScreen/LoginScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenApp.swift
+//  LoginScreen
+//
+//  Created by Mert Aydoğan on 14.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
